9d961a5fdf50943377be50d0d0cc7d52896a8c2a
Zachary-Smith-NL <zacharyrsmith.work@gmail.com>
2020-12-03 17:51:38 -0330

    Accidentally overwrote part1, so I rewrote it

--- day3/part1.py
+++ day3/part1.py
@@ -1,24 +1,13 @@
 def day_three(filename="input"):
-    slope_sum = []
+    slope_sum = 0
     #first read in each line, stripping it of the newline character, and splitting down to the only tile that will be checked
-    for i in range(5):
-        slope_sum.append(0)
-        input_file = open(filename, "r")
-        lines = input_file.readlines()
-        counter = 0
-        if i == 4:
-            down = 2
-        else:
-            down = 1
-        right = (1 + (2 * i)) % 8
-        for line_num in range(0, len(lines), down):
-            line = lines[line_num].split("\n")[0]
-            if(line[counter] == "#"):
-                slope_sum[i] += 1
-            counter = (counter + right) % len(line)
-    slope_total = 1
-    for value in slope_sum:
-        slope_total *= value
-    print(slope_total)
+    input_file = open(filename, "r")
+    counter = 0
+    for line in input_file:
+        line = line.split("\n")[0]
+        if(line[counter] == "#"):
+            slope_sum += 1
+        counter = (counter + 3) % len(line)
+    print(slope_sum)
 
 day_three()
