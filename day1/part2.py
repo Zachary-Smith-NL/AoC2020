cd98656d256e45e610acf2f5574bc5d35521a614
Zachary-Smith-NL <zacharyrsmith.work@gmail.com>
2020-12-03 12:40:17 -0330

    Finished part2, less overengineered this time

---
+++ day1/part2.py
@@ -0,0 +1,20 @@
+def day_one_2(filename="input"):
+    input_file = open(filename, "r")
+
+    #Less overengineering this time
+    values = []
+    for line in input_file:
+        value = int(line.split("\n")[0])
+        values.append(value)
+    for value in values:
+        for value2 in values:
+            if value == value2:
+                continue
+            for value3 in values:
+                if value == value3 or value2 == value3:
+                    continue
+                if value + value2 + value3 == 2020:
+                    #If values aren't the same & add up to 2020
+                    print(value * value2 * value3)
+                    return
+day_one_2()
